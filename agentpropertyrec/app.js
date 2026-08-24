89880a6c6807a25f8055c6bf7a5288d112ea2d09
Fiqih Amrullah <amrullahfiqih@gmail.com>
2026-08-25 02:04:16 +0700

    Add CORS Handler

--- agentpropertyrec/app.js
+++ agentpropertyrec/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import recommendationsRouter from "./src/routes/recommendations.js";
 
 const app = express();
 
+app.use(cors()); 
 app.use(express.json());
 app.use("/api", recommendationsRouter);
 
